aa607bb6253932eba1d0794ca85522935faff104
Bhupen Gupta <bhupengupta05@gmail.com>
2024-02-14 12:36:17 +0530

    Adding a token to the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Notify from './components/Notify'
 import { ALL_PERSONS } from './queries'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
@@ -21,9 +24,28 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if(!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage}/>
+        <h2>Login</h2>
+        <LoginForm
+        setToken={setToken}
+        setError={notify} />
+      </>
+    )
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage}/>
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify}/>
       <PhoneForm setError={notify}/>
