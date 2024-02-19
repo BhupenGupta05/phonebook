9e9821f169fa170da7bda504f426600d86ee428c
Bhupen Gupta <bhupengupta05@gmail.com>
2024-02-19 13:11:43 +0530

    Solved n+1 problem

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -14,10 +14,11 @@ const resolvers = {
       },
 
       allPersons: async (root, args) => {
+        console.log('Person.find');
         if(!args.phone) {
-          return Person.find({})
+          return Person.find({}).populate('friendOf')
         }
-        return Person.find({phone: {$exists: args.phone === 'YES'}})
+        return Person.find({phone: {$exists: args.phone === 'YES'}}).populate('friendOf')
       },
       findPerson: async (root, args) => Person.findOne({name: args.name})
     },
@@ -29,7 +30,17 @@ const resolvers = {
           city,
         }
       },
-    },
+
+      // friendOf: async (root) => {
+      //     const friends = await User.find({
+      //       friends: {
+      //         $in: [root._id]
+      //       }
+      //     })
+      //     console.log('User.find');
+      //     return friends
+      //   }
+      },
 
     Subscription: {
       personAdded: {
